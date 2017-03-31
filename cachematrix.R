0d1001d8ed83fc6f66b36b311952c6ea5a0a4682
ampgeronimo <ampgeronimo@gmail.com>
2017-03-31 22:46:29 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+	#assigns the value of m to NULL
+	m <- NULL
+	
+	#sets the value of the matrix
+	set <- function(y) {
+		#assigns input arguement(y) to x
+		x <<- y
+		#clears values of m that has been cached
+		m <<- NULL
+      }
+      
+	#gets the value of the matrix
+	get <- function() x
+      
+	#sets value of the inverse matrix
+	setmatrix <- function(matrix) m <<- matrix
+      
+	#gets the inverse matrix
+	getmatrix <- function() m
+      
+	#gives names to functions above
+	list(set = set,get = get,setmatrix = setmatrix,getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      #runs getmatrix function from makeCacheMatrix function
+	m <- x$getmatrix()
+      
+	#determines if there is a cached inverse matrix
+	if(!is.null(m)) {
+		message("getting cached data")
+      
+	      #returns the cached inverse matrix
+		return(m)
+      }
+	
+	#if there is no cached invere matrix,
+	#runs get function from makeCacheMatrix function
+      data <- x$get()
+
+	#solves inverse matrix
+      m <- solve(data, ...)
+
+	#runs setmatrix function with m as arguement 
+      x$setmatrix(m)
+
+	#print inverse matrix
+      m
 }
